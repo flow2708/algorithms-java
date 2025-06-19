ab61750d283deb3d68653599bf0fb9390d9f1f52
flow2708 <dibirovamin10@gmail.com>
2025-06-19 21:53:49 +0300

    Update Main.java

--- algorithms/breadth-first search/src/Main.java
+++ algorithms/breadth-first search/src/Main.java
@@ -10,25 +10,38 @@ public class Main {
     public boolean graph() {
         Hashtable<String, String[]> table = new Hashtable<>();
         String[] you = {"john","michael","alice","jennifer","james"};
+        String[] michael = {"steve", "robert"};
+        String[] jennifer = {"walter", "kate", "emma"};
 
         table.put("you", you);
+        table.put("michael", michael);
+        table.put("jennifer", jennifer);
 
         ArrayDeque<String> queue = new ArrayDeque();
-        //queue.add(Arrays.toString(you));
-        for (String s : table.get("you")) {
+        /*for (String s : table.get("you")) {
             queue.add(s);
         }
+         */
+        new Main().addToQueue(queue, table, "you");
 
         while (!queue.isEmpty()) {
             String person = queue.pop();
             if (new Main().isMangoSeller(person)) {
                 return true;
             }
+            if (table.get(person) != null) {
+                new Main().addToQueue(queue, table, person);
+            }
         }
         return false;
     }
+    public void addToQueue(Queue<String> queue, Hashtable<String, String[]> table, String key) {
+        for (String s : table.get(key)) {
+            queue.add(s);
+        }
+    }
     public boolean isMangoSeller(String name) {
-        if (name.equals("alice")) {
+        if (name.equals("kate")) {
             return true;
         } else {
             return false;
